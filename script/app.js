e7f7ee8a97a7ee6569b4aaac4bd66d585431d571
vitaliyaL <vita0623@mail.ru>
2022-05-07 01:27:22 +0300

    feat: task2 added

--- script/app.js
+++ script/app.js
@@ -3,3 +3,14 @@ function stringCutter(str) {
 }
 let firstSentence = "But you have no right to call me a murderer";
 console.log(stringCutter(firstSentence));
+
+function registerInvertion(str) {
+  let str1 = "";
+  for (let i = 0; i < str.length; i++) {
+    str[i] === str[i].toUpperCase()
+      ? (str1 += str[i].toLowerCase())
+      : (str1 += str[i].toUpperCase());
+  }
+  return str1;
+}
+console.log(registerInvertion("Be my Valentine"));
