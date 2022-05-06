29e372c4bfe3c295446f8c86314d0cdcc5143968
vitaliyaL <vita0623@mail.ru>
2022-05-07 01:28:25 +0300

    feat: task3 added

--- script/app.js
+++ script/app.js
@@ -14,3 +14,38 @@ function registerInvertion(str) {
   return str1;
 }
 console.log(registerInvertion("Be my Valentine"));
+
+function strObj(str) {
+  const length = str.length;
+  const obj = {
+    allSymbols: length,
+  };
+  str = str.trim();
+  let count = 0;
+  let symbols = 0;
+  for (let i = 0; i < str.length; i++) {
+    while (str[i] !== " " && i < str.length) {
+      symbols++;
+      i++;
+    }
+    count++;
+  }
+  obj.symbolsWithoutSpaces = symbols;
+  obj.quantityOfWords = count;
+  switch (str[str.length - 1]) {
+    case ".":
+      obj.sentenceType = "утвердительное";
+      break;
+    case "?":
+      obj.sentenceType = "вопросительное";
+      break;
+    case "!":
+      obj.sentenceType = "восклицательное";
+      break;
+    default:
+      obj.sentenceType = null;
+      break;
+  }
+  return obj;
+}
+console.log(strObj("  Я хочу учиться! "));
